767b908d46b4dc3e7c127f86bfddd330b42ce93a
aponceha <aponceha@gmail.com>
2023-04-03 21:33:26 -0400

    created Redux Cart Reducer and actions

---
+++ client/src/state/cart/cartReducer.js
@@ -0,0 +1,72 @@
+import {
+    ADD_TO_CART,
+    UPDATE_CART_QUANTITY,
+    REMOVE_FROM_CART,
+    ADD_MULTIPLE_TO_CART,
+    CLEAR_CART,
+    TOGGLE_CART,
+  } from './cartActions';
+  
+  const initialState = {
+    cart: [],
+    cartOpen: false,
+  };
+  
+  // TODO: To get a better understand of how a reducer works - add comments to the various actions in the reducer
+  export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_TO_CART:
+            return {
+              ...state,
+              cartOpen: true,
+              cart: [...state.cart, action.product],
+            };
+      
+          case ADD_MULTIPLE_TO_CART:
+            return {
+              ...state,
+              cart: [...state.cart, ...action.products],
+            };
+          // TODO: Add a comment describing the functionality of the UPDATE_CART_QUANTITY case
+          // Your comment here
+          case UPDATE_CART_QUANTITY:
+            return {
+              ...state,
+              cartOpen: true,
+              cart: state.cart.map((product) => {
+                if (action._id === product._id) {
+                  product.purchaseQuantity = action.purchaseQuantity;
+                }
+                return product;
+              }),
+            };
+      
+          // TODO: Add a comment describing the functionality of the REMOVE_FROM_CART case
+          // Your comment here
+          case REMOVE_FROM_CART:
+            let newState = state.cart.filter((product) => {
+              return product._id !== action._id;
+            });
+      
+            return {
+              ...state,
+              cartOpen: newState.length > 0,
+              cart: newState,
+            };
+      
+          case CLEAR_CART:
+            return {
+              ...state,
+              cartOpen: false,
+              cart: [],
+            };
+      
+          case TOGGLE_CART:
+            return {
+              ...state,
+              cartOpen: !state.cartOpen,
+            };
+      default:
+        return state;
+    }
+  };
